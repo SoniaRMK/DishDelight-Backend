de1326388bccea0ecc2a7655834e22eea1676527
KarungiRMS <KarungiRMSonia@gmail.com>
2024-11-15 18:02:28 +0300

    added password to DB config

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(cors());
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
